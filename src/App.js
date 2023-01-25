dc0ad866a392afc2b34dae114c895ae1bcb34b04
Mmmustapha42 <111079800+Mmmustapha42@users.noreply.github.com>
2023-01-26 00:19:58 +0100

    useeffect hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react';
+
 
 function App() {
+  const [resourseType, setResourceType] = useState('posts')
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourseType}`)
+      .then(res => res.json())
+      .then(data => setItems(data))
+
+  }, [resourseType])
+
+  function tryPost() {
+    setResourceType('posts')
+  }
+
+  function tryUsers() {
+    setResourceType('users')
+  }
+   
+  function tryComments(){
+    setResourceType('comments')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button onClick={tryPost}>Posts</button>
+    <button onClick={tryUsers}>Users</button>
+    <button onClick={tryComments}>Comments</button>
+
+    <h1>{resourseType}</h1>
+    {items.map(item =>{
+      return <pre>{JSON.stringify(item)}</pre>
+    })}
+    </>
   );
 }
 
